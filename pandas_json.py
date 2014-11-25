ce5f5051ee23d2b2c401f22b6862c06369a5ef5d
David Konerding <dek@google.com>
2014-11-25 13:23:03 -0800

    cleanups, switch to hdf.

--- pandas_json.py
+++ pandas_json.py
@@ -1,20 +1,19 @@
+import os
 import pandas
 import json
 import numpy
-path="c:/Users/dek/Desktop/example_weather.json"
-output="c:/Users/dek/Desktop/example_weather.csv"
-output2="c:/Users/dek/Desktop/example_weather_dates.csv"
+base = "/usr/local/google/home/dek/d3webview/app/src/main/assets/static_html"
+path = os.path.join(base, "example_weather.json")
+hdf_output = os.path.join(base, "example_weather.hf5")
 
-#d=pandas.io.json.read_json(path)
 data = json.load(open(path))
-d=pandas.io.json.json_normalize(data["weatherdata"])
-d.to_csv(output)
-d2=pandas.read_csv(output,index_col=0,parse_dates=["created_at", "collected_at"])
-#d2.index=[d2['created_at'],d2['uuid']]
-#d2 = d2.drop('created_at.1',1)
-#d2 = d2.drop('created_at',1)
-#d2 = d2.drop('uuid',1)
-d2.to_csv(output2)
-
-import code
-code.interact(local=locals())
+d = pandas.io.json.json_normalize(data["weatherdata"])
+d["collected_at"] = pandas.to_datetime(d["collected_at"])
+d["created_at"] = pandas.to_datetime(d["created_at"])
+d.set_index(d["created_at"])
+# Can't be serialized due to differing dtypes.
+d.drop("raw", 1, inplace=True)
+d.drop("version", 1, inplace=True)
+store = pandas.HDFStore(hdf_output, mode="w")
+store.put("data", d)
+store.close()
---
+++ pandas_read_hdf.py
@@ -0,0 +1,34 @@
+import pandas
+
+import os
+import pandas
+import json
+
+base = '/usr/local/google/home/dek/d3webview/app/src/main/assets/static_html'
+path = os.path.join(base, 'example_weather.hf5')
+
+store = pandas.HDFStore(path)
+
+for item in store.items():
+  print item
+d2 = store.get('data')
+store.close()
+
+
+d2 = pandas.read_hdf(path, 'data')
+# d2 = d2.drop('Unnamed: 0', 1)
+
+# x = d2.loc[:, ['outside_temp']]
+# in gnuplot:
+# set xdata time
+# set timefmt "%Y-%m-%d %H:%M:%S"
+# set datafile separator ","
+# plot "c:/Users/dek/Desktop/test.csv" every ::1 using 1:2:3 with points palette
+
+# d2[['outside_temp', 'uuid']].to_csv(
+#     'c:/Users/dek/Desktop/test2.csv', na_rep='NaN')
+
+
+pandas.set_option('display.width', None)
+import code
+code.interact(local=locals())
