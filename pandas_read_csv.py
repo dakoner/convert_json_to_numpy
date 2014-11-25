e5f04667829f0b3f0740f316b41d6e950cfd1251
David Konerding <dakoner@gmail.com>
2014-11-24 20:29:53 -0800

    extract data for plotting

--- pandas_read_csv.py
+++ pandas_read_csv.py
@@ -11,6 +11,15 @@ d2.index = pandas.to_datetime(d2['created_at'])
 d2 = d2.drop('created_at',1)
 #d2 = d2.drop('uuid',1)
 d2 = d2.drop('Unnamed: 0',1)
+x=d2.loc[:,['outside_temp']]
+x.to_csv("c:/Users/dek/Desktop/test.csv")
+# in gnuplot:
+# set xdata time
+# set timefmt "%Y-%m-%d %H:%M:%S"
+# set datafile separator ","
+# plot "c:/Users/dek/Desktop/test.csv" every ::1 using 1:2:3 with points palette
+
+d2[['outside_temp','uuid']].to_csv("c:/Users/dek/Desktop/test2.csv",na_rep='NaN')
 
 print(d2.keys())
 print(d2.index.names)
