c2a376c39115db2ea0a97502a45f3abc2b78bcdf
David Konerding <dakoner@gmail.com>
2014-11-25 19:30:22 -0800

    add example with very nice plot.

--- pandas_read_hdf.py
+++ pandas_read_hdf.py
@@ -2,7 +2,9 @@ import pandas
 
 import os
 import pandas
-import json
+from matplotlib import pyplot
+
+
 src = "c:/Users/dek/Projects"
 base = os.path.join(src, "d3webview/app/src/main/assets/static_html")
 path = os.path.join(base, 'example_weather.hf5')
@@ -25,6 +27,18 @@ store.close()
 # d2[['outside_temp', 'uuid']].to_csv(
 #     'c:/Users/dek/Desktop/test2.csv', na_rep='NaN')
 
+
+## This will resample two stations at 5Min interval, then merge the resampled.
+#u1=d2[d2['uuid']==1][['uuid','outside_temp']].resample('5Min');u1['uuid']=1
+#u3=d2[d2['uuid']==3][['uuid','outside_temp']].resample('5Min');u3['uuid']=3
+#pandas.concat([u1, u3]).sort_index()
+
+x=d2[['created_at','uuid','outside_temp']]
+y=x.pivot_table(index = 'created_at', columns='uuid').resample('5Min')
+
+#pyplot.figure()
+y.plot()
+pyplot.show()
 
 pandas.set_option('display.width', 235)
 import code
